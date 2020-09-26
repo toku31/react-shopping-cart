48783fd5cb1fae5475644a702acef69ab133b549
garden627 <garden627@yahoo.co.jp>
2020-09-26 18:15:07 +0900

    add animation and modal

--- src/App.js
+++ src/App.js
@@ -10,16 +10,20 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
-      cartItems: [],
+      cartItems: localStorage.getItem("cartItems")? JSON.parse(localStorage.getItem("cartItems")): [],
       size: "",
       sort: "",
     };
+  }
+  createOrder = (order) => {
+    alert("Need to save order for " + order.name);
   }
   removeFromCart = (product) => {
     const cartItems = this.state.cartItems.slice(); 
     this.setState({
       cartItems: cartItems.filter((x) => x._id !==product._id),
-    })
+    });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems.filter((x) => x._id !==product._id)));
   }
   
   addToCart = (product) => {
@@ -35,6 +39,7 @@ class App extends React.Component {
         cartItems.push({...product, count: 1 });
       }
      this.setState({cartItems}); 
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
   };
 
   sortProducts=(event)=>{
@@ -81,12 +86,12 @@ class App extends React.Component {
               <Products products={this.state.products} addToCart={this.addToCart}></Products>
             </div>
             <div className="sidebar">
-              <Cart cartItems = {this.state.cartItems} removeFromCart={this.removeFromCart}/>
+              <Cart cartItems = {this.state.cartItems} removeFromCart={this.removeFromCart} createOrder={this.createOrder}/>
             </div>
           </div>
         </main>
         <footer>
-          All right is reserved
+          All right is reserved.
         </footer>
       </div>
     );
