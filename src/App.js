1271e3d09eada00a38f959a955feb7cf4f26e65c
garden627 <garden627@yahoo.co.jp>
2020-10-10 23:55:34 +0900

    add redux to filter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // feature 1
 import React from 'react';
-import data from './data.json';
+// import data from './data.json';
 import Products from './components/Products';
 import Filter from './components/Filter';
 import Cart from './components/Cart';
@@ -11,12 +11,12 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      products: data.products,
+      // products: data.products,  // Redux導入後は不要
       cartItems: localStorage.getItem('cartItems')
         ? JSON.parse(localStorage.getItem('cartItems'))
         : [],
-      size: '',
-      sort: '',
+      // size: '',
+      // sort: '',
     };
   }
   createOrder = (order) => {
@@ -49,44 +49,44 @@ class App extends React.Component {
     localStorage.setItem('cartItems', JSON.stringify(cartItems));
   };
 
-  sortProducts = (event) => {
-    console.log(event.target.value);
-    const sort = event.target.value;
-    // imp
-    this.setState((state) => ({
-      sort: sort,
-      products: this.state.products
-        .slice()
-        .sort((a, b) =>
-          sort === 'lowest'
-            ? a.price > b.price
-              ? 1
-              : -1
-            : sort === 'highest'
-            ? a.price < b.price
-              ? 1
-              : -1
-            : a._id < b._id
-            ? 1
-            : -1
-        ),
-    }));
-  };
+  // sortProducts = (event) => {
+  //   console.log(event.target.value);
+  //   const sort = event.target.value;
+  //   // imp
+  //   this.setState((state) => ({
+  //     sort: sort,
+  //     products: this.state.products
+  //       .slice()
+  //       .sort((a, b) =>
+  //         sort === 'lowest'
+  //           ? a.price > b.price
+  //             ? 1
+  //             : -1
+  //           : sort === 'highest'
+  //           ? a.price < b.price
+  //             ? 1
+  //             : -1
+  //           : a._id < b._id
+  //           ? 1
+  //           : -1
+  //       ),
+  //   }));
+  // };
 
-  filterProducts = (event) => {
-    // impl
-    console.log(event.target.value);
-    if (event.target.value === '') {
-      this.setState({ size: event.target.value, products: data.products });
-    } else {
-      this.setState({
-        size: event.target.value,
-        products: data.products.filter(
-          (product) => product.availableSizes.indexOf(event.target.value) >= 0
-        ),
-      });
-    }
-  };
+  // filterProducts = (event) => {
+  //   // impl
+  //   console.log(event.target.value);
+  //   if (event.target.value === '') {
+  //     this.setState({ size: event.target.value, products: data.products });
+  //   } else {
+  //     this.setState({
+  //       size: event.target.value,
+  //       products: data.products.filter(
+  //         (product) => product.availableSizes.indexOf(event.target.value) >= 0
+  //       ),
+  //     });
+  //   }
+  // };
 
   render() {
     return (
@@ -99,14 +99,14 @@ class App extends React.Component {
             <div className='content'>
               <div className='main'>
                 <Filter
-                  count={this.state.products.length}
-                  size={this.state.size}
-                  sort={this.state.sort}
-                  filterProducts={this.filterProducts}
-                  sortProducts={this.sortProducts}
+                // count={this.state.products.length}
+                // size={this.state.size}
+                // sort={this.state.sort}
+                // filterProducts={this.filterProducts}
+                // sortProducts={this.sortProducts}
                 ></Filter>
                 <Products
-                  products={this.state.products}
+                  // products={this.state.products}
                   addToCart={this.addToCart}
                 ></Products>
               </div>
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,31 +1,68 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { filterProducts, sortProducts } from '../actions/productActions';
+import { connect } from 'react-redux';
 
-export default class Filter extends Component {
-    render() {
-        return (
-            <div className="filter">
-                <div className="filter-result">{this.props.count} Products</div>
-                <div className="filter-sort">
-                    Order{" "}
-                    <select value={this.props.sort} onChange={this.props.sortProducts}>
-                        <option>Latest</option>
-                        <option value="lowest">Lowest</option>
-                        <option value="highest">Highest</option>
-                    </select>
-                </div>
-                <div className="filter-size">
-                    Filter{" "}
-                    <select value={this.props.size} onChange={this.props.filterProducts}>
-                        <option value="">ALL</option>
-                        <option value="XS">XS</option>
-                        <option value="S">S</option>
-                        <option value="M">M</option>
-                        <option value="L">L</option>
-                        <option value="XL">XL</option>
-                        <option value="XXL">XXL</option>
-                    </select>
-                </div>
-            </div>
-        );
-    }
+// Redux作成により修正
+// export default class Filter extends Component {
+class Filter extends Component {
+  render() {
+    return !this.props.filteredProducts ? (
+      <div>Loading...</div>
+    ) : (
+      <div className='filter'>
+        <div className='filter-result'>
+          {this.props.filteredProducts.length} Products
+        </div>
+        <div className='filter-sort'>
+          Order{' '}
+          {/* <select value={this.props.sort} onChange={this.props.sortProducts}> */}
+          <select
+            value={this.props.sort}
+            onChange={(e) =>
+              this.props.sortProducts(
+                this.props.filteredProducts,
+                e.target.value
+              )
+            }
+          >
+            <option value='latest'>Latest</option>
+            <option value='lowest'>Lowest</option>
+            <option value='highest'>Highest</option>
+          </select>
+        </div>
+        <div className='filter-size'>
+          Filter{' '}
+          {/* <select value={this.props.size} onChange={this.props.filterProducts}> */}
+          <select
+            value={this.props.size}
+            onChange={(e) =>
+              this.props.filterProducts(this.props.products, e.target.value)
+            }
+          >
+            <option value=''>ALL</option>
+            <option value='XS'>XS</option>
+            <option value='S'>S</option>
+            <option value='M'>M</option>
+            <option value='L'>L</option>
+            <option value='XL'>XL</option>
+            <option value='XXL'>XXL</option>
+          </select>
+        </div>
+      </div>
+    );
+  }
 }
+// Redux作成により以下追記
+export default connect(
+  (state) => ({
+    // connect props: state <=>props
+    size: state.products.size,
+    sort: state.products.sort,
+    products: state.products.items,
+    filteredProducts: state.products.filteredItems,
+  }),
+  {
+    filterProducts, // mapping actions
+    sortProducts,
+  }
+)(Filter); //component name
