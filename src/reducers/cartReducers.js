631ccea6b3630f52e8861481baf32ee8dc47429c
garden627 <garden627@yahoo.co.jp>
2020-11-01 18:35:03 +0900

    create order

--- src/reducers/cartReducers.js
+++ src/reducers/cartReducers.js
@@ -2,7 +2,7 @@ import { ADD_TO_CART, REMOVE_FROM_CART } from "../types";
 
 
 export const cartReducer = (
-  state = { cartItems: JSON.parse(localStorage.getItem("cartItems") || "{}")}, action) => {
+  state = { cartItems: JSON.parse(localStorage.getItem("cartItems") || "[]")}, action) => {
   switch (action.type) {
     case ADD_TO_CART:
       return { cartItems: action.payload.cartItems }  // update
